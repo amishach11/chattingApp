6bb6d2edd403f01055940088a5e6cd52bcf83b56
amishach11 <Amishachopra21@gmail.com>
2022-01-10 15:19:40 +0530

    Updates

---
+++ precache-manifest.b83e1f15b711cc2961801cc290a08c20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb59f38f956061e421c9846fe03f2fa",
+    "url": "/ChattingApp/index.html"
+  },
+  {
+    "revision": "59ef1ee5be82a70b0b53",
+    "url": "/ChattingApp/static/css/2.b9c16c38.chunk.css"
+  },
+  {
+    "revision": "7292549d439255aa1917",
+    "url": "/ChattingApp/static/css/main.05767668.chunk.css"
+  },
+  {
+    "revision": "59ef1ee5be82a70b0b53",
+    "url": "/ChattingApp/static/js/2.5ff69868.chunk.js"
+  },
+  {
+    "revision": "920fa96ef36f6a853e121581309e3564",
+    "url": "/ChattingApp/static/js/2.5ff69868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7292549d439255aa1917",
+    "url": "/ChattingApp/static/js/main.70e0e162.chunk.js"
+  },
+  {
+    "revision": "d96167339e5bf4af23d5",
+    "url": "/ChattingApp/static/js/runtime-main.936f03c4.js"
+  }
+]);
